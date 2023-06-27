6724fbb1039590397b6276f6847287f7478886c2
vvsjsatheesh <137255710+vvsjsatheesh@users.noreply.github.com>
2023-06-27 12:00:15 +0000

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,12 @@
+variable "instance_type" {
+  description = "The EC2 instance type"
+  type        = string
+}
+variable "subnet_id" {
+  description = "The subnet"
+  type        = string
+}
+variable "ami_id" {
+  description = "ami_id"
+  type        = string
+}
